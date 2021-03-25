37a529d72bc7cda6170ebf8036a29e2696f31e4b
Tobi Dalhof <dalhof.tobias@gmail.com>
2021-03-25 18:42:04 +0000

    add hello world code

--- 000-hello-world/hello.go
+++ 000-hello-world/hello.go
@@ -1,1 +1,5 @@
 package hello
+
+func Hello(name string) string {
+	return "Hello, " + name
+}
--- 000-hello-world/hello_test.go
+++ 000-hello-world/hello_test.go
@@ -1,1 +1,11 @@
 package hello
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello("test")
+	want := "Hello, test"
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
