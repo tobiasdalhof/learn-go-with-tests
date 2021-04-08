87ec8d0a06af43e96078ac7accc892f344a3e506
Tobi Dalhof <dalhof.tobias@gmail.com>
2021-04-08 14:50:33 +0000

    add counter code

---
+++ 011-sync/counter_test.go
@@ -0,0 +1,42 @@
+package counter
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestConter(t *testing.T) {
+	t.Run("increment the counter 3 times", func(t *testing.T) {
+		counter := NewCounter()
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		assertCounter(t, counter, 3)
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func(w *sync.WaitGroup) {
+				counter.Inc()
+				w.Done()
+			}(&wg)
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
+}
+
+func assertCounter(t testing.TB, counter *Counter, want int) {
+	t.Helper()
+	if counter.Value() != want {
+		t.Errorf("got %d, want %d", counter.Value(), want)
+	}
+}
