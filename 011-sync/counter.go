afa6ee40641e011d122bae51880f0b80bffeaac6
Tobi Dalhof <dalhof.tobias@gmail.com>
2021-04-09 07:58:25 +0000

    add chan example

--- 011-sync/counter.go
+++ 011-sync/counter.go
@@ -7,15 +7,32 @@ type Counter struct {
 	value int
 }
 
+// type Counter struct {
+// 	inc   chan int
+// 	value int
+// }
+
 func NewCounter() *Counter {
 	return &Counter{}
 }
+
+// func NewCounter() *Counter {
+// 	return &Counter{inc: make(chan int, 1)}
+// }
 
 func (c *Counter) Inc() {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	c.value++
 }
+
+// func (c *Counter) Inc() {
+// 	c.inc <- 1
+// 	select {
+// 	case <-c.inc:
+// 		c.value++
+// 	}
+// }
 
 func (c *Counter) Value() int {
 	return c.value
